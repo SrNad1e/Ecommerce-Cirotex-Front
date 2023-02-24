2025c8b4574a9b052e3246d1cfb01b138c962499
camilo <kaito290421@gmail.com>
2023-02-24 09:50:51 -0500

    se organiza proceso de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,8 @@
 import { GoogleOutlined, ShoppingOutlined } from "@ant-design/icons"
-import { Button, Col, Divider, Form, Input, Modal, Row, Typography } from "antd"
+import { Alert, Button, Col, Divider, Form, Input, Modal, Row, Typography } from "antd"
+import { useForm } from "antd/es/form/Form"
 import FormItem from "antd/es/form/FormItem"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const { Text } = Typography
 
@@ -13,23 +14,31 @@ type Props = {
 
 
 const Login = ({ visible, onCancel, setIsLogged }: Props) => {
-    const userLogin = () => {
-        fetch('http://localhost:3000/auth/login', {
+    const [data, setData] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+
+    const [form] = useForm()
+
+
+    const userLogin = async () => {
+        const formValues = await form.getFieldsValue()
+        await fetch('http://localhost:8080/auth/login', {
             method: 'POST',
             body: JSON.stringify({
-                username: "MrNobody",
-                password: "1234",
+                username: formValues.username,
+                password: formValues.password,
             }),
             headers: {
                 "Content-type": "application/json"
             },
             mode: 'cors'
-        }).then(response => response.json()).then(json => console.log(json)).then(() => setIsLogged(true)).then(() => onCancel())
+        }).then(response => response.json()).then((json) => console.log(json),
+         /*json.access_token ? setIsLogged(true) : setIsLogged(false)*/).then(() => onCancel())
     }
 
     return (
         <Modal width={400} open={visible} onCancel={onCancel} footer={<></>}>
-            <Form layout="vertical" >
+            <Form form={form} layout="vertical" >
                 <Row justify={'center'} gutter={[0, 10]} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <Col>
                         <ShoppingOutlined style={{ fontSize: 45 }} />
@@ -39,12 +48,12 @@ const Login = ({ visible, onCancel, setIsLogged }: Props) => {
                         <Text style={{ fontSize: 15 }} italic strong >Bienvenido, Accede para una mejor experiencia</Text>
                     </Col>
                     <Col span={24}>
-                        <FormItem >
+                        <FormItem name='username'>
                             <Input placeholder="Ingrese nombre de usuario" />
                         </FormItem>
                     </Col>
                     <Col span={24}>
-                        <FormItem >
+                        <FormItem name='password' >
                             <Input placeholder="Ingrese contraseña" />
                         </FormItem>
                     </Col>
